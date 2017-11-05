c828c5c7bb8e08d144134458082eb9eb351827ae
robertosalazare <jrse95@gmail.com>
2017-11-05 09:09:33 -0800

    Cambios a la función de utilidad en el othello

--- othello.py
+++ othello.py
@@ -22,28 +22,6 @@ class Othello(JuegoSumaCeros2T):
         Se inicializa el tablero como una matriz de 8x8
         donde llena de 0,1 y -1, donde 0 es un lugar vacío,
         1 es una pieza blanca y -1 es una pieza negra.
-
-        este es mi caso de prueba no le haga caso.
-        (
-            [ 0, 0, 0, 0, 0, 0, 0, 0],
-            [ 0, 0, 0, 0,-1, 0, 0, 0],
-            [ 0, 0, 0, 0,-1, -1, 0, 0],
-            [ 0, 0, 1, 1,-1, 1,-1, 0],
-            [ 0, 0, 0,-1,-1, 1, 1, 0],
-            [ 0, 0,-1, 1, 1, 1, 0, 0],
-            [ 0, 0, 0,-1,-1, 1,-1, 0],
-            [ 0, 0, 0, 0, 0, 0, 0, 0]
-        )
-        (
-            [ 0, 0, 0, 0, 0, 0, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0, 0],
-            [ 0, 0, 0, 1,-1, 0, 0, 0],
-            [ 0, 0, 0,-1, 1, 0, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0, 0]
-        )
         """
         super().__init__((
              0, 0, 0, 0, 0, 0, 0, 0,
@@ -220,7 +198,7 @@ class Othello(JuegoSumaCeros2T):
 
         self.jugador *= -1
 
-def utilidad(x):
+def numero_piezas(x):
     """
     En este método solamente se cuentan cuantas negras y cuantas blancas hay
     se restan y luego se dividen entre la suma, una forma medio macana pero
@@ -230,6 +208,30 @@ def utilidad(x):
     blancas,negras = x.count(1),x.count(-1)
 
     return (blancas - negras)/(blancas + negras)
+
+def movilidad(x):
+    juego = Othello()
+    juego.x = x
+    movilidad_negras = len(juego.jugadas_legales())
+    juego.jugador = 1
+    movilidad_blancas = len(juego.jugadas_legales())
+
+    return (movilidad_blancas - movilidad_negras)/(movilidad_blancas + movilidad_negras)
+
+def esquinas(X):
+    esquinas_blancas,esquinas_negras = 0
+    for i in (0,7,56,63):
+        if x[i] == 1:
+            esquinas_blancas += 1
+        elif x[i] == -1:
+            esquinas_negras += -1
+    
+    esquinas = 0 if esquinas_blancas + esquinas_negras == 0 else (esquinas_blancas - esquinas_negras)/(esquinas_blancas + esquinas_negras)
+    
+    return esquinas
+
+def utilidad(x):
+    return 0.4*esquinas(x) + 0.4*movilidad(x) + 0.2*numero_piezas(x)
 
 def ordena_jugadas(juego):
     jugadas = list(juego.jugadas_legales())
@@ -244,15 +246,19 @@ class JuegoOthello:
         juego = Othello()
         os.system('cls' if os.name == 'nt' else 'clear')
         print("Juego de othello contra una máquina con el algoritmo minimax.\n")
+        print("En este juego siempre empiezan las negras: ")
+        print("X: Piezas negras.")
+        print("O: Piezas blancas.\n")
         res = ""
 
         while res != "s" and res != "n":
             res = input("¿Quieres ser primeras(s/n)?")
 
         if res == "n":
+            os.system('cls' if os.name == 'nt' else 'clear')
             juego.dibuja_tablero()
             print("Esperando el movimiento de la máquina...")
-            jugada = minimax(juego, dmax=6, utilidad=utilidad,
+            jugada = minimax(juego, dmax=6, utilidad=utilidad_2,
                                 ordena_jugadas=ordena_jugadas,
                                 transp={})
 
@@ -285,7 +291,7 @@ class JuegoOthello:
             jugadas = juego.jugadas_legales()
 
             if len(jugadas) > 0:
-                jugada = minimax(juego, dmax=6, utilidad=utilidad,
+                jugada = minimax(juego, dmax=5, utilidad=utilidad,
                                 ordena_jugadas=ordena_jugadas,
                                 transp={})
 
