c4eb811871ae9b5494ee7cb331e5f61350c92160
jjups96 <jjups96@gmail.com>
2018-04-14 14:27:45 -0700

    In spanish

--- othello.py
+++ othello.py
@@ -14,28 +14,28 @@ __author__ = 'Jordan Urias'
 class othello():
     def __init__(self, _n=8):
         self.n = _n
-        self.board = [['0' for x in range(self.n)] for y in range(self.n)]
-        # 8 directions
+        self.tablero = [['0' for x in range(self.n)] for y in range(self.n)]
+        # 8 direcciones
         self.dirx = [-1,  0,  1, -1, 1, -1, 0, 1]
         self.diry = [-1, -1, -1,  0, 0,  1, 1, 1]
         
-        if self.n % 2 == 0: # if board size is even
+        if self.n % 2 == 0: # Que sea un tablero par
             z = int((self.n - 2) / 2)
-            self.board[z][z] = '2'
-            self.board[self.n - 1 - z][z] = '1'        
-            self.board[z][self.n - 1 - z] = '1'
-            self.board[self.n - 1 - z][self.n - 1 - z] = '2'
+            self.tablero[z][z] = '2'
+            self.tablero[self.n - 1 - z][z] = '1'        
+            self.tablero[z][self.n - 1 - z] = '1'
+            self.tablero[self.n - 1 - z][self.n - 1 - z] = '2'
     
     
-    def PrintBoard(self):
+    def PrintTablero(self):
         m = len(str(self.n - 1))
         for y in range(self.n):
-            row = ''
+            renglon = ''
             for x in range(self.n):
-                row += self.board[y][x]
-                row += ' ' * m
-            print (row + ' ' + str(y))
-        row = ''
+                renglon += self.tablero[y][x]
+                renglon += ' ' * m
+            print (renglon + ' ' + str(y))
+        renglon = ''
         for x in range(self.n):
-            row += str(x).zfill(m) + ' '
-        print (row + '\n')        
+            renglon += str(x).zfill(m) + ' '
+        print (renglon + '\n')        
