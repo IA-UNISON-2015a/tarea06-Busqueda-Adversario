ca67450f6ef7f68530a00441a72b104d50ec0d00
jjups96 <jjups96@gmail.com>
2018-04-14 13:58:43 -0700

    Revert "Othello. Propio"
    This reverts commit 127b6cbb91706ba5a2a491a3b1d80e8125b0e4c9.

--- othello.py
+++ othello.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from busquedas_adversarios import JuegoSumaCeros2T
+from busquedas_adversarios import minimax
+import tkinter as tk
+from random import shuffle
+import time
+
 """
 othello.py
 ------------
 
-El juego de Otello implementado por ustedess mismos, con jugador inteligente
+El juego de Otello implementado por ustes mismos, con jugador inteligente
 
 """
 
 __author__ = 'Jordan Urias'
 
-class othello():
-    def __init__(self, _n=8):
-        self.n = _n
-        self.board = [['0' for x in range(self.n)] for y in range(self.n)]
-        # 8 directions
-        self.dirx = [-1,  0,  1, -1, 1, -1, 0, 1]
-        self.diry = [-1, -1, -1,  0, 0,  1, 1, 1]
+class othello(JuegoSumaCeros2T):
+    def __init__(self):
+        #Posicion inicial del tablero
+        x= [0 for _ in range(64)]
+        x[27]=-1
+        x[28]=1
+        x[35]=1
+        x[36]=-1
         
-        if self.n % 2 == 0: # if board size is even
-            z = int((self.n - 2) / 2)
-            self.board[z][z] = '2'
-            self.board[self.n - 1 - z][z] = '1'        
-            self.board[z][self.n - 1 - z] = '1'
-            self.board[self.n - 1 - z][self.n - 1 - z] = '2'
-    
-    
-    def PrintBoard(self):
-        m = len(str(self.n - 1))
-        for y in range(self.n):
-            row = ''
-            for x in range(self.n):
-                row += self.board[y][x]
-                row += ' ' * m
-            print (row + ' ' + str(y))
-        row = ''
-        for x in range(self.n):
-            row += str(x).zfill(m) + ' '
-        print (row + '\n')        
+        super().__init__(tuple(x),1)
+        
+    def terminal(self):
+        negras = self.x.count(1)
+        blancas = self.x.count(-1)
+        
+        #Si fue eliminado
+        if negras == 0 or blancas == 0:
+            return 1 if blancas > negras else -1
+
+        #Si aun hay lugares.
+        if 0 in self.x:
+            return None
+
+        #1 si ganan las blancas -1 si ganan las negras 0 en empate.
+        return 1 if blancas > negras else -1 if negras > blancas else 0
