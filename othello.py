d59e40df5ac9118a968a71a97fda7d9c0517f00d
jjups96 <jjups96@gmail.com>
2018-04-14 16:12:58 -0700

    Othello. Utilidad

--- othello.py
+++ othello.py
@@ -38,4 +38,27 @@ class othello():
         renglon = ''
         for x in range(self.n):
             renglon += str(x).zfill(m) + ' '
-        print (renglon + '\n')        
+            print (renglon + '\n')        
+    
+    '''
+     [4, 2, 2, 2, 2, 2, 2, 4],
+     [2, 1, 1, 1, 1, 1, 1, 2],
+     [2, 1, 1, 1, 1, 1, 1, 2],
+     [2, 1, 1, 1, 1, 1, 1, 2],
+     [2, 1, 1, 1, 1, 1, 1, 2],
+     [2, 1, 1, 1, 1, 1, 1, 2],
+     [2, 1, 1, 1, 1, 1, 1, 2],
+     [4, 2, 2, 2, 2, 2, 2, 4]]
+    '''
+    def UtilidadTablero(self, tablero, jugador):
+        utilidad = 0
+        for y in range(self.n):
+            for x in range(self.n):
+                if tablero[y][x] == jugador:
+                    if (x == 0 or x == self.n - 1) and (y == 0 or y == self.n - 1):
+                        utilidad += 4 # Esquinas
+                    elif (x == 0 or x == self.n - 1) or (y == 0 or y == self.n - 1):
+                        utilidad += 2 # Lados
+                    else:
+                        utilidad += 1
+        return utilidad
