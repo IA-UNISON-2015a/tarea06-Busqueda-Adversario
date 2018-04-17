a9a8c0d6c1bb7150f52f6a147b283940747563e6
jjups96 <jjups96@gmail.com>
2018-04-16 22:36:56 -0700

    Othello. Push Final Tryhard
    Se agrego numpy, se copiaron los pseudocodigos de wikipedia relacionados con el minimax

--- othello.py
+++ othello.py
@@ -13,14 +13,17 @@ __author__ = 'Jordan Urias'
 
 import copy
 import os
+import numpy as np
+from time import perf_counter
 
 class othello():
     def __init__(self, _n=8):
         self.n = _n
-        self.tablero = [['0' for x in range(self.n)] for y in range(self.n)]
+        auxTab = [['0' for x in range(self.n)] for y in range(self.n)]
+        self.tablero = np.asarray(auxTab)
         # 8 direcciones
-        self.dirx = [-1,  0,  1, -1, 1, -1, 0, 1]
-        self.diry = [-1, -1, -1,  0, 0,  1, 1, 1]
+        self.dirx = np.asarray([-1,  0,  1, -1, 1, -1, 0, 1])
+        self.diry = np.asarray([-1, -1, -1,  0, 0,  1, 1, 1])
         self.minUtilidadTablero = -1 
         self.maxUtilidadTablero = self.n * self.n + 4 * self.n + 4 + 1 
         
@@ -43,7 +46,7 @@ class othello():
         renglon = ''
         for x in range(self.n):
             renglon += str(x).zfill(m) + ' '
-        print (renglon + '\n')        
+        print ('\n' + renglon )        
     
     '''
      [4, 2, 2, 2, 2, 2, 2, 4],
@@ -105,6 +108,16 @@ class othello():
         if numFichasOpp == 0:
             return False
         return True
+    '''
+    X Y, a jugar
+    '''
+    def movLegales(self, tablero, jugador):
+        jugadas = []
+        for y in range(self.n):
+            for x in range(self.n):
+                if self.esLegal(tablero, x, y, jugador):
+                    jugadas.append([x,y])
+        return np.asarray(jugadas)
     
     def esJugadaTerminal(self, tablero, jugador):
         for y in range(self.n):
@@ -112,7 +125,9 @@ class othello():
                 if self.esLegal(tablero, x, y, jugador):
                     return False
         return True
-
+    '''
+    Tablero despues de hacer jugada
+    '''
     def getJugadaOrd(self, tablero, jugador):
         jugadasOrdenadas = []
         for y in range(self.n):
@@ -122,28 +137,23 @@ class othello():
                     jugadasOrdenadas.append((tableroTemp, self.UtilidadTablero(tableroTemp, jugador)))
         jugadasOrdenadas = sorted(jugadasOrdenadas, key = lambda node: node[1], reverse = True)
         jugadasOrdenadas = [node[0] for node in jugadasOrdenadas]
-        return jugadasOrdenadas
+        return np.asarray(jugadasOrdenadas)
     
-    
+  
     def Minimax(self, tablero, jugador, prof, maximizarjugador):
         if prof == 0 or self.esJugadaTerminal(tablero, jugador):
             return self.UtilidadTablero(tablero, jugador)
+        jugadasOrdenadas = self.getJugadaOrd(tablero, jugador)
         if maximizarjugador:
-            mejorUtilidad = self.minUtilidadlTablero
-            for y in range(self.n):
-                for x in range(self.n):
-                    if self.esLegal(tablero, x, y, jugador):
-                        (tableroTemp, numFichasOpp) = self.hacerMoviento(copy.deepcopy(tablero), x, y, jugador)
-                        v = self.Minimax(tableroTemp, jugador, prof - 1, False)
-                        mejorUtilidad = max(mejorUtilidad, v)
+            mejorUtilidad = self.minUtilidadTablero
+            for tableroTemp in jugadasOrdenadas:
+                utilidad = self.Minimax(tableroTemp, jugador, prof - 1, False)
+                mejorUtilidad = max(mejorUtilidad, utilidad)
         else: # minimizargjugador
-            mejorUtilidad = self.maxUtilidadlTablero
-            for y in range(self.n):
-                for x in range(self.n):
-                    if self.esLegal(tablero, x, y, jugador):
-                        (tableroTemp, numFichasOpp) = self.hacerMoviento(copy.deepcopy(tablero), x, y, jugador)
-                        v = self.Minimax(tableroTemp, jugador, prof - 1, True)
-                        mejorUtilidad = min(mejorUtilidad, v)
+            mejorUtilidad = self.maxUtilidadTablero
+            for tableroTemp in jugadasOrdenadas:
+                utilidad = self.Minimax(tableroTemp, jugador, prof - 1, True)
+                mejorUtilidad = min(mejorUtilidad, utilidad)
         return mejorUtilidad
     
     def AlphaBeta(self, tablero, jugador, prof, alpha, beta, maximizarjugador):
@@ -151,23 +161,54 @@ class othello():
             return self.UtilidadTablero(tablero, jugador)
         jugadasOrdenadas = self.getJugadaOrd(tablero, jugador)
         if maximizarjugador:
-            v = self.minUtilidadTablero
+            utilidad = self.minUtilidadTablero
             for tableroTemp in jugadasOrdenadas:
-                v = max(v, self.AlphaBeta(tableroTemp, jugador, prof - 1, alpha, beta, False))
-                alpha = max(alpha, v)
+                utilidad = max(utilidad, self.AlphaBeta(tableroTemp, jugador, prof - 1, alpha, beta, False))
+                alpha = max(alpha, utilidad)
                 if beta <= alpha:
                     break # poda de beta
-            return v
+            return utilidad
         else: # minimizar jugador
-            v = self.maxUtilidadTablero
+            utilidad = self.maxUtilidadTablero
             for tableroTemp in jugadasOrdenadas:
-                v = min(v, self.AlphaBeta(tableroTemp, jugador, prof - 1, alpha, beta, True))
-                beta = min(beta, v)
+                utilidad = min(utilidad, self.AlphaBeta(tableroTemp, jugador, prof - 1, alpha, beta, True))
+                beta = min(beta, utilidad)
                 if beta <= alpha:
                     break # poda de alpha
-            return v
-
-
+            return utilidad
+        
+    def NegamaxAB(self,tablero, jugador, prof, alpha, beta, color):
+        if prof == 0 or self.esJugadaTerminal(tablero, jugador):
+            return color * self.UtilidadTablero(tablero, jugador)
+        jugadasOrdenadas = self.getJugadaOrd(tablero, jugador)
+        mejorUtilidad = self.minUtilidadTablero
+        for tableroTemp in jugadasOrdenadas:
+            utilidad = -self.NegamaxAB(tableroTemp, jugador, prof - 1, -beta, -alpha, -color)
+            mejorUtilidad = max(mejorUtilidad, utilidad)
+            alpha = max(alpha, utilidad)
+            if alpha >= beta:
+                break
+        return mejorUtilidad
+    
+    def Negascout(self, tablero, jugador, prof, alpha, beta, color):
+        if prof == 0 or self.esJugadaTerminal(tablero, jugador):
+            return color * self.UtilidadTablero(tablero, jugador)
+        jugadasOrdenadas = self.getJugadaOrd(tablero, jugador)
+        primerHijo = True
+        for tableroTemp in jugadasOrdenadas:
+            if not primerHijo:
+                utilidad = -self.Negascout(tableroTemp, jugador, prof - 1, -alpha - 1, -alpha, -color)
+                if alpha < utilidad and utilidad < beta:
+                    utilidad = -self.Negascout(tableroTemp, jugador, prof - 1, -beta, -utilidad, -color)
+            else:
+                primerHijo = False
+                utilidad = -self.Negascout(tableroTemp, jugador, prof - 1, -beta, -alpha, -color)
+            alpha = max(alpha, utilidad)
+            if alpha >= beta:
+                break
+        return alpha
+    
+    
     def MejorMovimientoUtilidad(self,tablero, jugador, prof):
         maxpuntos = 0
         mx = -1; my = -1
@@ -207,12 +248,57 @@ class othello():
                         mx = x; my = y
         return (mx, my)
     
+    def MejorMovimientoNegamax(self,tablero, jugador, prof):
+        maxpuntos = 0
+        mx = -1; my = -1
+        for y in range(self.n):
+            for x in range(self.n):
+                if self.esLegal(tablero, x, y, jugador):
+                    (tableroTemp, numFichasOpp) = self.hacerMoviento(copy.deepcopy(tablero), x, y, jugador)
+                    puntos = self.NegamaxAB(tableroTemp, jugador, prof, self.minUtilidadTablero, self.maxUtilidadTablero, 1)
+                    if puntos > maxpuntos:
+                        maxpuntos = puntos
+                        mx = x; my = y
+        return (mx, my)
+    
+    def MejorMovimientoNegascout(self,tablero, jugador, prof):
+        maxpuntos = 0
+        mx = -1; my = -1
+        for y in range(self.n):
+            for x in range(self.n):
+                if self.esLegal(tablero, x, y, jugador):
+                    (tableroTemp, numFichasOpp) = self.hacerMoviento(copy.deepcopy(tablero), x, y, jugador)
+                    puntos = self.Negascout(tableroTemp, jugador, prof, self.minUtilidadTablero, self.maxUtilidadTablero, 1)
+                    if puntos > maxpuntos:
+                        maxpuntos = puntos
+                        mx = x; my = y
+        return (mx, my)
+    
+    def minimax_t(self,tablero, jugador,tmax=5):
+
+        bf = len(list(tablero.jugadas_legales()))
+        t_ini = perf_counter()
+        for d in range(2, 50):
+            ta = perf_counter()
+            jugada = self.MejorMovimientoMinimax(tablero,jugador, d)
+            tb = perf_counter()
+            if bf * (tb - ta) > t_ini + tmax - tb:
+                return jugada
+    
 if __name__ == '__main__':
     juego=othello()
     prof = 4
+    
+    print("Juego Othello que realmente es el reversi.")
+    print("Dentro del juego, si quieres salir solo presiona enter.")
+    print("Esta onda no valida entradas. Dale la posicion asi: X Y")
+    print("Si quieres jugar de segundo, cambia el codigo.")
+    
+    prof = int(input("Profundidad (Dificultad): "))
+    
+    
     while True:
         for p in range(2):
-            print
             juego.PrintTablero()
             jugador = str(p + 1)
             print ('jugador: ' + jugador)
@@ -223,6 +309,8 @@ if __name__ == '__main__':
                 os._exit(0)            
             if jugador == '1':
                 while True:
+                    #Para los n00bs que no saben jugar
+                    #print("Jugadas posibles: \n",juego.movLegales(juego.tablero,jugador),'\n')
                     xy = input('X Y: ')
                     if xy == '': os._exit(0)
                     (x, y) = xy.split()
@@ -234,7 +322,7 @@ if __name__ == '__main__':
                     else:
                         print ('Movimiento invalido!')
             else: # 
-                (x, y) = juego.MejorMovimientoAlphaBeta(juego.tablero, jugador,prof)
+                (x, y) = juego.MejorMovimientoNegascout(juego.tablero, jugador,prof)
                 if not (x == -1 and y == -1):
                     (juego.tablero, numFichasOpp) = juego.hacerMoviento(juego.tablero, x, y, jugador)
                     print ('AI jugo (X Y): ' + str(x) + ' ' + str(y))
