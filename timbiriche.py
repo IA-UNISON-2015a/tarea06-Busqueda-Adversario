b9c7ca165e4d43d75ba613c5f284d2f83cb20890
Irving Borboa <iborboa50@gmail.com>
2019-04-29 15:44:46 -0700

    clase timbiriche

--- timbiriche.py
+++ timbiriche.py
@@ -38,6 +38,44 @@ class Timbiriche(JuegoSumaCeros2T):
             if self.x[posicion] == 0:
                 return posicion
             
+    def terminal(self):
+        x = self.x
+        for i in range(3):
+            if x[i] != 0 and (x[i] == x[i+ 3] == x[i + 4] == x[i+7]):
+                return x[i]
+        for i in range(7,10):
+            if x[i] != 0 and (x[i] == x[i+ 3] == x[i + 4] == x[i+7]):
+                return x[i]
+        for i in range(14,17):
+            if x[i] != 0 and (x[i] == x[i+ 3] == x[i + 4] == x[i+7]):
+                return x[i]
+        for i in range(21,24):
+            if x[i] != 0 and (x[i] == x[i+ 3] == x[i + 4] == x[i+7]):
+                return x[i]
+        if 0 not in self.x:
+            return 0
+        return None
+    def hacer_jugada(self,jugada):
+        for i in range(30):
+            if self.x[self.jugador] == 0:
+                self.x[i + jugada] = self.jugador
+                self.historial.append(jugada)
+                self.jugador *= -1
+                return None
+    def deshacer_jugada(self,jugada):
+        pos = self.historial.pop()
+        for i in range(30):
+            if self.x[pos] != 0:
+                self.x[pos] = 0
+                self.jugador *= -1
+                return None
+            
+def utilidad(x):
+    
+        
+    
+    
+    
     
         
     
